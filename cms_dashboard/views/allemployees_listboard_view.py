c30bcd56251d8132d5a872bf4409075385270618
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-09-01 08:16:11 +0200

    Adding new listboards

---
+++ cms_dashboard/views/allemployees_listboard_view.py
@@ -0,0 +1,51 @@
+import re
+# from django.apps import apps as django_apps
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.utils.decorators import method_decorator
+from edc_base.view_mixins import EdcBaseViewMixin
+from edc_dashboard.view_mixins import ListboardFilterViewMixin, SearchFormViewMixin
+from edc_dashboard.views import ListboardView
+from edc_navbar import NavbarViewMixin
+
+from ..model_wrappers import EmployeeModelWrapper
+
+
+class AllEmployeesListBoardView(NavbarViewMixin, EdcBaseViewMixin,
+                    ListboardFilterViewMixin, SearchFormViewMixin, ListboardView):
+
+    listboard_template = 'allemployees_listboard_template'
+    listboard_url = 'allemployees_listboard_url'
+    listboard_panel_style = 'success'
+    listboard_fa_icon = "fa fa-user-circle"
+
+    model = 'contract.employee'
+    model_wrapper_cls = EmployeeModelWrapper
+    navbar_name = 'cms_dashboard'
+    navbar_selected_item = 'employee'
+    ordering = '-modified'
+    paginate_by = 10
+    search_form_url = 'allemployees_listboard_url'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            employee_add_url=self.model_cls().get_absolute_url())
+        return context
+
+    def get_queryset_filter_options(self, request, *args, **kwargs):
+        options = super().get_queryset_filter_options(request, *args, **kwargs)
+        if kwargs.get('identifier'):
+            options.update(
+                {'identifier': kwargs.get('identifier')})
+        return options
+
+    def extra_search_options(self, search_term):
+        q = Q()
+        if re.match('^[A-Z]+$', search_term):
+            q = Q(first_name__exact=search_term)
+        return q
