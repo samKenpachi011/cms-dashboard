b9d7bdde825d98b7d7e60464c41a924e42a11e42
tumiemosweu <imosweuk@gmail.com>
2021-05-10 14:19:01 +0200

    update KPA model wrappers

--- cms_dashboard/model_wrappers/professional_skills_model_wrapper_mixin.py
+++ cms_dashboard/model_wrappers/professional_skills_model_wrapper_mixin.py
@@ -12,11 +12,17 @@ class StrategicOrientationModelWrapperMixin(ProfessionalSkillsWrapperMixin):
     strategic_model_cls = 'bhp_personnel.strategicorientation'
     strategic_wrapper_cls = StrategicOrientationModelWrapper
 
+    def strategic_model_obj(self):
+        return super().model_obj(self.strategic_model_cls)
+
 
 class ResultsFocusModelWrapperMixin(ProfessionalSkillsWrapperMixin):
 
     results_focus_model_cls = 'bhp_personnel.resultsfocus'
     results_focus_wrapper_cls = ResultsFocusModelWrapper
+
+    def results_focus_obj(self):
+        return super().model_obj(self.results_focus_model_cls)
 
 
 class LeadershipAndMotivationModelWrapperMixin(ProfessionalSkillsWrapperMixin):
@@ -24,11 +30,17 @@ class LeadershipAndMotivationModelWrapperMixin(ProfessionalSkillsWrapperMixin):
     leadership_model_cls = 'bhp_personnel.leadershipandmotivation'
     leadership_wrapper_cls = LeadershipAndMotivationModelWrapper
 
+    def leadership_and_motivation_obj(self):
+        return super().model_obj(self.leadership_model_cls)
+
 
 class InnovationAndCreativityModelWrapperMixin(ProfessionalSkillsWrapperMixin):
 
     innovation_model_cls = 'bhp_personnel.innovationandcreativity'
     innovation_wrapper_cls = InnovationAndCreativityModelWrapper
+
+    def innovation_obj(self):
+        return super().model_obj(self.innovation_model_cls)
 
 
 class PlanningSkillsModelWrapperMixin(ProfessionalSkillsWrapperMixin):
@@ -36,11 +48,17 @@ class PlanningSkillsModelWrapperMixin(ProfessionalSkillsWrapperMixin):
     planning_skills_model_cls = 'bhp_personnel.planningskills'
     planning_skills_wrapper_cls = PlanningSkillsModelWrapper
 
+    def planning_skills_obj(self):
+        return super().model_obj(self.planning_skills_model_cls)
+
 
 class InterpersonalSkillsModelWrapperMixin(ProfessionalSkillsWrapperMixin):
 
     interpersonal_skills_model_cls = 'bhp_personnel.interpersonalskills'
     interpersonal_skills_wrapper_cls = InterpersonalSkillsModelWrapper
+
+    def interpersonal_obj(self):
+        return super().model_obj(self.interpersonal_skills_model_cls)
 
 
 class CommunicationSkillsModelWrapperMixin(ProfessionalSkillsWrapperMixin):
@@ -48,14 +66,23 @@ class CommunicationSkillsModelWrapperMixin(ProfessionalSkillsWrapperMixin):
     communication_skills_model_cls = 'bhp_personnel.communicationskills'
     communication_skills_wrapper_cls = CommunicationSkillsModelWrapper
 
+    def communication_obj(self):
+        return super().model_obj(self.communication_skills_model_cls)
+
 
 class KnowledgeAndProductivityModelWrapperMixin(ProfessionalSkillsWrapperMixin):
 
     knowledge_model_cls = 'bhp_personnel.knowledgeandproductivity'
     knowledge_wrapper_cls = KnowledgeAndProductivityModelWrapper
 
+    def knowledge_obj(self):
+        return super().model_obj(self.knowledge_model_cls)
+
 
 class QualityOfWorkModelWrapperMixin(ProfessionalSkillsWrapperMixin):
 
     quality_of_work_model_cls = 'bhp_personnel.qualityofwork'
     quality_of_work_wrapper_cls = QualityOfWorkModelWrapper
+
+    def quality_model_obj(self):
+        return super().model_obj(self.quality_of_work_model_cls)
